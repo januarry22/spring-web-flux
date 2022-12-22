a4783b656c2ca88b252afd907e8a3ed976971236
januarry22 <tls2330@naver.com>
2022-12-22 22:21:05 +0900

    Spring cloud config Client

--- src/main/java/com/januarry22/webflux/RunWebFluxApplication.java
+++ src/main/java/com/januarry22/webflux/RunWebFluxApplication.java
@@ -2,6 +2,7 @@ package com.januarry22.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
